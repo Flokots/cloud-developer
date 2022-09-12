1684406d39dffac2d42675139f811d64142fa020
FloKots <florencekotohoyoh@gmail.com>
2022-09-12 07:38:33 +0300

    move to a separate folder

---
+++ udacity-c2-restapi/src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  "dev": {
+    "username": "udagramflodev",
+    "password": "kenya232s",
+    "database": "udagramflodev",
+    "host": "udagramflodev.c1ee1k6otqfj.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-flonight-dev"
+  },
+  "jwt": {
+    "secret": " "
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram-flonight-dev",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
